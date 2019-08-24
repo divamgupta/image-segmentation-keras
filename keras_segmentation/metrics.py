8afb0e93cc05ef4dd3c30c7b38893aa106467fa6
Edwin <economical.gaming18@gmail.com>
2019-08-24 13:33:57 -0500

    Se agregan las métricas de Jaccard y Ternaus

--- keras_segmentation/metrics.py
+++ keras_segmentation/metrics.py
@@ -1,12 +1,36 @@
 import numpy as np
+from keras import backend as K
+from keras.losses import binary_crossentropy
 
-EPS = 1e-12
+eps = 1e-12
 
-def get_iou( gt , pr , n_classes ):
-	class_wise = np.zeros(n_classes)
-	for cl in range(n_classes):
-		intersection = np.sum(( gt == cl )*( pr == cl ))
-		union = np.sum(np.maximum( ( gt == cl ) , ( pr == cl ) ))
-		iou = float(intersection)/( union + EPS )
-		class_wise[ cl ] = iou
-	return class_wise
+
+def get_iou(gt, pr, n_classes):
+    class_wise = np.zeros(n_classes)
+    for cl in range(n_classes):
+        intersection = np.sum((gt == cl) * (pr == cl))
+        union = np.sum(np.maximum((gt == cl), (pr == cl)))
+        iou = float(intersection) / (union + eps)
+        class_wise[cl] = iou
+    return class_wise
+
+
+def indice_jaccard(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    interseccion = K.sum(y_true_f * y_pred_f)
+    union = K.sum(y_true_f) + K.sum(y_pred_f)
+    resultado = (interseccion + 1.0) / (union - interseccion + 1.0)
+
+    return K.mean(resultado)
+
+
+def ternaus_loss(y_true, y_pred):
+    loss = binary_crossentropy(y_true, y_pred) - K.log(indice_jaccard(y_true, y_pred))
+
+    return loss
+
+
+def loss_jaccard(y_true, y_pred):
+
+    return -indice_jaccard(y_true, y_pred)
