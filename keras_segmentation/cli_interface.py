5062dcd3fea1b50eeaed3ce6555b9ad7fe51909d
Rounaq Jhunjhunu wala <rounaqwl66@gmail.com>
2019-09-22 11:13:56 +0530

    Improve the command line interface
    - Use argparse subparsers for all commands
    - Add an entrypoint for execution via the command line

--- keras_segmentation/cli_interface.py
+++ keras_segmentation/cli_interface.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 
 import sys
 import argparse
@@ -10,9 +10,8 @@ from . import data_utils
 from .data_utils.visualize_dataset import visualize_segmentation_dataset
 
 
-def cli_train():
-
-    parser = argparse.ArgumentParser()
+def train_action(command_parser):
+    parser = command_parser.add_parser('train')
     parser.add_argument("command", type=str)
     parser.add_argument("--model_name", type=str, required=True)
     parser.add_argument("--train_images", type=str, required=True)
@@ -38,93 +37,93 @@ def cli_train():
     parser.add_argument("--steps_per_epoch", type=int, default=512)
     parser.add_argument("--optimizer_name", type=str, default="adadelta")
 
-    args = parser.parse_args()
+    def action(args):
+        return train.train(model=args.model_name,
+                    train_images=args.train_images,
+                    train_annotations=args.train_annotations,
+                    input_height=args.input_height,
+                    input_width=args.input_width,
+                    n_classes=args.n_classes,
+                    verify_dataset=args.not_verify_dataset,
+                    checkpoints_path=args.checkpoints_path,
+                    epochs=args.epochs,
+                    batch_size=args.batch_size,
+                    validate=args.validate,
+                    val_images=args.val_images,
+                    val_annotations=args.val_annotations,
+                    val_batch_size=args.val_batch_size,
+                    auto_resume_checkpoint=args.auto_resume_checkpoint,
+                    load_weights=args.load_weights,
+                    steps_per_epoch=args.steps_per_epoch,
+                    optimizer_name=args.optimizer_name
+                    )
+    
+    parser.set_defaults(func=action)
 
-    train.train(model=args.model_name,
-                train_images=args.train_images,
-                train_annotations=args.train_annotations,
-                input_height=args.input_height,
-                input_width=args.input_width,
-                n_classes=args.n_classes,
-                verify_dataset=args.not_verify_dataset,
-                checkpoints_path=args.checkpoints_path,
-                epochs=args.epochs,
-                batch_size=args.batch_size,
-                validate=args.validate,
-                val_images=args.val_images,
-                val_annotations=args.val_annotations,
-                val_batch_size=args.val_batch_size,
-                auto_resume_checkpoint=args.auto_resume_checkpoint,
-                load_weights=args.load_weights,
-                steps_per_epoch=args.steps_per_epoch,
-                optimizer_name=args.optimizer_name
-                )
+def predict_action(command_parser):
 
-
-def cli_predict():
-
-    parser = argparse.ArgumentParser()
+    parser = command_parser.add_parser('predict')
     parser.add_argument("command", type=str)
     parser.add_argument("--checkpoints_path", type=str, required=True)
     parser.add_argument("--input_path", type=str, default="", required=True)
     parser.add_argument("--output_path", type=str, default="", required=True)
 
-    args = parser.parse_args()
+    def action(args):
+        input_path_extension = args.input_path.split('.')[-1]
+        if input_path_extension in ['jpg', 'jpeg', 'png']:
+            return predict.predict(inp=args.input_path, out_fname=args.output_path,
+                            checkpoints_path=args.checkpoints_path)
+        else:
+            return predict.predict_multiple(inp_dir=args.input_path,
+                                    out_dir=args.output_path,
+                                    checkpoints_path=args.checkpoints_path)
+    
+    parser.set_defaults(func=action)
 
-    if ".jpg" in args.input_path or ".png" in args.input_path or \
-            ".jpeg" in args.input_path:
-        predict.predict(inp=args.input_path, out_fname=args.output_path,
-                        checkpoints_path=args.checkpoints_path)
-    else:
-        predict.predict_multiple(inp_dir=args.input_path,
-                                 out_dir=args.output_path,
-                                 checkpoints_path=args.checkpoints_path)
 
+def verify_dataset_action(command_parser):
 
-def cli_verify_dataset():
-
-    parser = argparse.ArgumentParser()
+    parser = command_parser.add_parser('verify_dataset')
     parser.add_argument("command", type=str)
     parser.add_argument("--images_path", type=str)
     parser.add_argument("--segs_path", type=str)
     parser.add_argument("--n_classes", type=int)
 
-    args = parser.parse_args()
+    def action(args):
+        data_utils.data_loader.verify_segmentation_dataset(
+            args.images_path, args.segs_path, args.n_classes)
+    
+    parser.set_defaults(func=action)
 
-    data_utils.data_loader.verify_segmentation_dataset(
-        args.images_path, args.segs_path, args.n_classes)
 
+def visualize_dataset_action(command_parser):
 
-def cli_visualize_dataset():
-
-    parser = argparse.ArgumentParser()
+    parser = command_parser.add_parser('visualize_dataset')
     parser.add_argument("command", type=str)
     parser.add_argument("--images_path", type=str)
     parser.add_argument("--segs_path", type=str)
     parser.add_argument("--n_classes", type=int)
     parser.add_argument('--do_augment', action='store_true')
 
-    args = parser.parse_args()
+    def action(args):
+        visualize_segmentation_dataset(args.images_path, args.segs_path,
+                                    args.n_classes, do_augment=args.do_augment)
 
-    visualize_segmentation_dataset(args.images_path, args.segs_path,
-                                   args.n_classes, do_augment=args.do_augment)
-
+    parser.set_defaults(func=action)
 
 def main():
     assert len(sys.argv) >= 2, \
         "python -m keras_segmentation <command> <arguments>"
 
-    command = sys.argv[1]
+    main_parser = argparse.ArgumentParser()
+    command_parser = main_parser.add_subparsers()
+    
+    # Add individual commands
+    train_action(command_parser)
+    predict_action(command_parser)
+    verify_dataset_action(command_parser)
+    visualize_dataset_action(command_parser)
 
-    if command == "train":
-        cli_train()
-    elif command == "predict":
-        cli_predict()
-    elif command == "verify_dataset":
-        cli_verify_dataset()
-    elif command == "visualize_dataset":
-        cli_visualize_dataset()
-    else:
-        print("Invalid command ", command)
+    args = main_parser.parse_args()
 
-    print(command)
+    args.func(args)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(name="keras_segmentation",
-      version="0.2.0",
+      version="0.3.0",
       description="Image Segmentation toolkit for keras",
       author="Divam Gupta",
       author_email='divamgupta@gmail.com',
@@ -10,4 +10,9 @@ setup(name="keras_segmentation",
       license="MIT",
       url="https://github.com/divamgupta/image-segmentation-keras",
       packages=find_packages(),
+      entry_points={
+            'console_scripts': [
+                  'keras_segmentation = keras_segmentation.__main__:main'
+            ]
+      }
       )
