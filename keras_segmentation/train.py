fb59ebce01c040d3eea86ca6e8842c22ed662136
Marius-Juston <Marius.juston@hotmail.fr>
2021-01-16 22:27:42 -0500

    uses tensorflow's simpler lastest_checkpoint function

--- keras_segmentation/train.py
+++ keras_segmentation/train.py
@@ -1,36 +1,13 @@
 import json
 from .data_utils.data_loader import image_segmentation_generator, \
     verify_segmentation_dataset
-import glob
 import six
 from keras.callbacks import Callback
 from tensorflow.keras.callbacks import ModelCheckpoint
+import tensorflow as tf
 
-def find_latest_checkpoint(checkpoints_path, fail_safe=True):
-
-    def get_epoch_number_from_path(path):
-        return path.replace(checkpoints_path, "").strip(".")
-
-    # Get all matching files
-    all_checkpoint_files = glob.glob(checkpoints_path + ".*")
-    all_checkpoint_files = [ ff.replace(".index" , "" ) for ff in all_checkpoint_files ] # to make it work for newer versions of keras
-    # Filter out entries where the epoc_number part is pure number
-    all_checkpoint_files = list(filter(lambda f: get_epoch_number_from_path(f)
-                                       .isdigit(), all_checkpoint_files))
-    if not len(all_checkpoint_files):
-        # The glob list is empty, don't have a checkpoints_path
-        if not fail_safe:
-            raise ValueError("Checkpoint path {0} invalid"
-                             .format(checkpoints_path))
-        else:
-            return None
-
-    # Find the checkpoint file with the maximum epoch
-    latest_epoch_checkpoint = max(all_checkpoint_files,
-                                  key=lambda f:
-                                  int(get_epoch_number_from_path(f)))
-    return latest_epoch_checkpoint
-
+def find_latest_checkpoint(checkpoints_path):
+    return tf.train.latest_checkpoint(checkpoints_path)
 
 def masked_categorical_crossentropy(gt, pr):
     from keras.losses import categorical_crossentropy
