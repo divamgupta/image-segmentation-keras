feccace330006ccd811ec00c46f9a3616d9979fa
Divam Gupta <divamgupta@gmail.com>
2021-03-03 03:04:55 -0500

    fix checkpoint bug

--- keras_segmentation/train.py
+++ keras_segmentation/train.py
@@ -11,31 +11,31 @@ import glob
 import sys
 
 def find_latest_checkpoint(checkpoints_path, fail_safe=True):
-    
-      # This is legacy code, there should always be a "checkpoint" file in your directory
 
-      def get_epoch_number_from_path(path):
-          return path.replace(checkpoints_path, "").strip(".")
+    # This is legacy code, there should always be a "checkpoint" file in your directory
 
-      # Get all matching files
-      all_checkpoint_files = glob.glob(checkpoints_path + ".*")
-      all_checkpoint_files = [ff.replace(".index", "") for ff in
-                              all_checkpoint_files]  # to make it work for newer versions of keras
-      # Filter out entries where the epoc_number part is pure number
-      all_checkpoint_files = list(filter(lambda f: get_epoch_number_from_path(f)
-                                         .isdigit(), all_checkpoint_files))
-      if not len(all_checkpoint_files):
-          # The glob list is empty, don't have a checkpoints_path
-          if not fail_safe:
-              raise ValueError("Checkpoint path {0} invalid"
-                               .format(checkpoints_path))
-          else:
-              return None
+    def get_epoch_number_from_path(path):
+        return path.replace(checkpoints_path, "").strip(".")
 
-      # Find the checkpoint file with the maximum epoch
-      latest_epoch_checkpoint = max(all_checkpoint_files,
-                                    key=lambda f:
-                                    int(get_epoch_number_from_path(f)))
+    # Get all matching files
+    all_checkpoint_files = glob.glob(checkpoints_path + ".*")
+    all_checkpoint_files = [ff.replace(".index", "") for ff in
+                            all_checkpoint_files]  # to make it work for newer versions of keras
+    # Filter out entries where the epoc_number part is pure number
+    all_checkpoint_files = list(filter(lambda f: get_epoch_number_from_path(f)
+                                       .isdigit(), all_checkpoint_files))
+    if not len(all_checkpoint_files):
+        # The glob list is empty, don't have a checkpoints_path
+        if not fail_safe:
+            raise ValueError("Checkpoint path {0} invalid"
+                             .format(checkpoints_path))
+        else:
+            return None
+
+    # Find the checkpoint file with the maximum epoch
+    latest_epoch_checkpoint = max(all_checkpoint_files,
+                                  key=lambda f:
+                                  int(get_epoch_number_from_path(f)))
 
     return latest_epoch_checkpoint
 
