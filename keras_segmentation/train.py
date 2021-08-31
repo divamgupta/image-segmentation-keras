bfaec91c807747c846f0fc688bd259c3dcf34c13
Sushant Agarwal <41518238+sushantag9@users.noreply.github.com>
2021-08-31 14:12:55 +0530

    Update train.py

--- keras_segmentation/train.py
+++ keras_segmentation/train.py
@@ -10,7 +10,14 @@ import tensorflow as tf
 from tensorflow.keras import backend as K
 import glob
 import sys
-from metrics import dice_coef
+
+
+def dice_coef(y_true, y_pred):
+
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + K.epsilon()) / (K.sum(y_true_f) + K.sum(y_pred_f) + K.epsilon())
 
 def dice_coef_loss(y_true, y_pred):
     return 1-dice_coef(y_true, y_pred)
