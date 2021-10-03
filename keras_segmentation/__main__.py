13c8ef8d94b81a3ff01742d5c61139de53c5632b
ahmed <ahmed_hani_dawoud@hotmail.com>
2021-10-03 14:46:05 -0400

    Return the output in memory

--- keras_segmentation/__main__.py
+++
@@ -1,7 +0,0 @@
-
-def main():
-    from . import cli_interface
-    cli_interface.main()
-
-if __name__ == "__main__":
-    main()
--- keras_segmentation/models/model.py
+++
@@ -1,6 +0,0 @@
-""" Definition for the generic Model class """
-
-
-class Model:
-    def __init__(self, n_classes, input_height=None, input_width=None):
-        pass
