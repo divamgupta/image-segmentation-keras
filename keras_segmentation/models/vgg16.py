0da793d6d706719610ee7b8663a428c2c63bbd40
pandya keval <kevalpandya175@gmail.com>
2021-11-04 02:43:32 +0530

    Update vgg16.py
    Nothing much but only fixing the  attribute not found error of keras.utils.get_file

--- keras_segmentation/models/vgg16.py
+++ keras_segmentation/models/vgg16.py
@@ -1,7 +1,7 @@
 import keras
 from keras.models import *
 from keras.layers import *
-
+import tensorflow as tf
 from .config import IMAGE_ORDERING
 
 if IMAGE_ORDERING == 'channels_first':
@@ -74,7 +74,7 @@ def get_vgg_encoder(input_height=224,  input_width=224, pretrained='imagenet', c
     f5 = x
 
     if pretrained == 'imagenet':
-        VGG_Weights_path = keras.utils.get_file(
+        VGG_Weights_path = tf.keras.utils.get_file(
             pretrained_url.split("/")[-1], pretrained_url)
         Model(img_input, x).load_weights(VGG_Weights_path, by_name=True, skip_mismatch=True)
 
