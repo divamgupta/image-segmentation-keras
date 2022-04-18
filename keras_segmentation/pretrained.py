450a7fd49003fd045e25852883be8ab5c8c5be47
mmahdim <m.m.m5651@gmail.com>
2022-04-18 23:49:15 +0430

    added tf.keras.utils option

--- keras_segmentation/pretrained.py
+++ keras_segmentation/pretrained.py
@@ -1,5 +1,5 @@
 import keras
-
+import tensorflow as tf
 from .models.all_models import model_from_name
 
 
@@ -12,7 +12,7 @@ def model_from_checkpoint_path(model_config, latest_weights):
     return model
 
 
-def resnet_pspnet_VOC12_v0_1():
+def resnet_pspnet_VOC12_v0_1(framework='keras'):
 
     model_config = {
         "output_height": 96,
@@ -26,13 +26,15 @@ def resnet_pspnet_VOC12_v0_1():
     REPO_URL = "https://github.com/divamgupta/image-segmentation-keras"
     MODEL_PATH = "pretrained_model_1/r2_voc12_resnetpspnet_384x576.24"
     model_url = "{0}/releases/download/{1}".format(REPO_URL, MODEL_PATH)
-    latest_weights = keras.utils.get_file(model_url.split("/")[-1], model_url)
-
+    if (framework == 'keras'):
+        latest_weights = keras.utils.get_file(model_url.split("/")[-1], model_url)
+    elif (framework == 'tf.keras'):
+        latest_weights = tf.keras.utils.get_file(model_url.split("/")[-1], model_url)
     return model_from_checkpoint_path(model_config, latest_weights)
 
 
 # pretrained model converted from caffe by Vladkryvoruchko ... thanks !
-def pspnet_50_ADE_20K():
+def pspnet_50_ADE_20K(framework='keras'):
 
     model_config = {
         "input_height": 473,
@@ -43,12 +45,15 @@ def pspnet_50_ADE_20K():
 
     model_url = "https://www.dropbox.com/s/" \
                 "0uxn14y26jcui4v/pspnet50_ade20k.h5?dl=1"
-    latest_weights = keras.utils.get_file("pspnet50_ade20k.h5", model_url)
+    if (framework == 'keras'):
+        latest_weights = keras.utils.get_file("pspnet50_ade20k.h5", model_url)
+    elif (framework == 'tf.keras'):
+        latest_weights = tf.keras.utils.get_file("pspnet50_ade20k.h5", model_url)
 
     return model_from_checkpoint_path(model_config, latest_weights)
 
 
-def pspnet_101_cityscapes():
+def pspnet_101_cityscapes(framework='keras'):
 
     model_config = {
         "input_height": 713,
@@ -59,12 +64,15 @@ def pspnet_101_cityscapes():
 
     model_url = "https://www.dropbox.com/s/" \
                 "c17g94n946tpalb/pspnet101_cityscapes.h5?dl=1"
-    latest_weights = keras.utils.get_file("pspnet101_cityscapes.h5", model_url)
-
+    if (framework == 'keras'):
+        latest_weights = keras.utils.get_file("pspnet101_cityscapes.h5", model_url)
+    elif (framework == 'tf.keras'):
+        latest_weights = tf.keras.utils.get_file("pspnet101_cityscapes.h5", model_url)
+        
     return model_from_checkpoint_path(model_config, latest_weights)
 
 
-def pspnet_101_voc12():
+def pspnet_101_voc12(framework='keras'):
 
     model_config = {
         "input_height": 473,
@@ -75,6 +83,9 @@ def pspnet_101_voc12():
 
     model_url = "https://www.dropbox.com/s/" \
                 "uvqj2cjo4b9c5wg/pspnet101_voc2012.h5?dl=1"
-    latest_weights = keras.utils.get_file("pspnet101_voc2012.h5", model_url)
+    if (framework == 'keras'):
+        latest_weights = keras.utils.get_file("pspnet101_voc2012.h5", model_url)
+    else:
+        latest_weights = tf.keras.utils.get_file("pspnet101_voc2012.h5", model_url)
 
     return model_from_checkpoint_path(model_config, latest_weights)
